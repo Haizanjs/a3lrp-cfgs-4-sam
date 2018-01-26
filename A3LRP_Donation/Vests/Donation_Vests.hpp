c89a9b4cf694835ba557b0c945013f6b681cc37b
SamLannister <icetexy@gmail.com>
2018-01-26 16:49:10 +0000

    shite

--- A3LRP_Donation/Vests/Donation_Vests.hpp
+++ A3LRP_Donation/Vests/Donation_Vests.hpp
@@ -1,25 +1,79 @@
-class A3LRP_Donation_Vest_Lannister: A3LRP_Donation_Vest_Base
+class A3LRP_LanV_King: A3LRP_Donation_Vest_Base
 {
 	scope=2;
-	displayName="A3LRP Plate Carrier (Lannister)";
+	displayName="House Lannister Vest (King)";
 	hiddenSelections[]={"camo"};
-	hiddenSelectionsTextures[]={"A3LRP_Donation\Vests\data\Vest_Carrier_Lannister.paa"};
+	hiddenSelectionsTextures[]={"A3LRP_Donation\Vests\data\King.paa"};
 };
 
 
-class A3LRP_Donation_Vest_Baratheon: A3LRP_Donation_Vest_Base
+class A3LRP_LanV_KingsHand: A3LRP_Donation_Vest_Base
 {
 	scope=2;
-	displayName="A3LRP Plate Carrier (Baratheon)";
+	displayName="House Lannister Vest (Kings Hand)";
 	hiddenSelections[]={"camo"};
-	hiddenSelectionsTextures[]={"A3LRP_Donation\Vests\data\Vest_Carrier_Baratheon.paa"};
+	hiddenSelectionsTextures[]={"A3LRP_Donation\Vests\data\Kings_Hand.paa"};
 };
 
 
-class A3LRP_Donation_Vest_TF141: A3LRP_Donation_Vest_Base
+class A3LRP_LanV_HighLord: A3LRP_Donation_Vest_Base
 {
 	scope=2;
-	displayName="A3LRP Plate Carrier (TF141)";
+	displayName="House Lannister Vest (High Lord)";
 	hiddenSelections[]={"camo"};
-	hiddenSelectionsTextures[]={"A3LRP_Donation\Vests\data\Vest_Carrier_TF141.paa"};
+	hiddenSelectionsTextures[]={"A3LRP_Donation\Vests\data\High_Lord.paa"};
+};
+
+
+class A3LRP_LanV_Lord: A3LRP_Donation_Vest_Base
+{
+	scope=2;
+	displayName="House Lannister Vest (Lord)";
+	hiddenSelections[]={"camo"};
+	hiddenSelectionsTextures[]={"A3LRP_Donation\Vests\data\Lord.paa"};
+};
+
+
+class A3LRP_LanV_ManAtArms: A3LRP_Donation_Vest_Base
+{
+	scope=2;
+	displayName="House Lannister Vest (Man at Arms)";
+	hiddenSelections[]={"camo"};
+	hiddenSelectionsTextures[]={"A3LRP_Donation\Vests\data\Man_at_Arms.paa"};
+};
+
+
+class A3LRP_LanV_KingsGuard: A3LRP_Donation_Vest_Base
+{
+	scope=2;
+	displayName="House Lannister Vest (Kings Guard)";
+	hiddenSelections[]={"camo"};
+	hiddenSelectionsTextures[]={"A3LRP_Donation\Vests\data\Kings_Guard.paa"};
+};
+
+
+class A3LRP_LanV_Serjeant: A3LRP_Donation_Vest_Base
+{
+	scope=2;
+	displayName="House Lannister Vest (Serjeant)";
+	hiddenSelections[]={"camo"};
+	hiddenSelectionsTextures[]={"A3LRP_Donation\Vests\data\Serjants.paa"};
+};
+
+
+class A3LRP_LanV_Footman: A3LRP_Donation_Vest_Base
+{
+	scope=2;
+	displayName="House Lannister Vest (Footman)";
+	hiddenSelections[]={"camo"};
+	hiddenSelectionsTextures[]={"A3LRP_Donation\Vests\data\Foot_Man.paa"};
+};
+
+
+class A3LRP_LanV_Lev: A3LRP_Donation_Vest_Base
+{
+	scope=2;
+	displayName="House Lannister Vest (Levy)";
+	hiddenSelections[]={"camo"};
+	hiddenSelectionsTextures[]={"A3LRP_Donation\Vests\data\Levy.paa"};
 };
