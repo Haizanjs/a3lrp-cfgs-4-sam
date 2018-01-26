a8051c2dcbf896528a8c77a7479926c37ac30bd9
Jack <jackmichaelsimons59@gmail.com>
2018-01-26 23:23:35 +0000

    sam

--- Lannister/Vehicles/config.cpp
+++ Lannister/Vehicles/config.cpp
@@ -5,7 +5,7 @@ class CfgPatches
 		units[]={};
 		weapons[]={};
 		requiredVersion=0.1;
-		requiredAddons[]={"LandRover","G65","GTR"};
+		requiredAddons[]={"LandRover","G65","GTR","Abruzzi_M5"};
 		version="2018-01-24";
 	};
 };
@@ -16,7 +16,9 @@ class cfgVehicles
 	class ALRP_Mustang;
 	class MercG65;
 	class NissanGTR;
+	class BMW_M5;
 	#include "\Lannister\Vehicles\MercG65\config.hpp"
 	#include "\Lannister\Vehicles\FordMustang\config.hpp"
 	#include "\Lannister\Vehicles\NissanGTR\config.hpp"
+	#include "\Lannister\Vehicles\BMW_M5\config.hpp"
 };
