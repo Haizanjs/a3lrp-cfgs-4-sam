827cc1a3ca5d13619ec3d604bc9a98a1c6800b15
SamLannister <icetexy@gmail.com>
2018-01-26 23:55:24 +0000

    reeeeee3

--- Lannister/Vehicles/BMW_M5/config.hpp
+++ Lannister/Vehicles/BMW_M5/config.hpp
@@ -3,5 +3,13 @@ class A3LRP_LanV_Red_BMW_M5 : BMW_M5
 	scope = 2;
 	displayName = "A3LRP BMW M5 (House Lannister Red)";
 	hiddenSelections[] = {"jump","lb-left-front-corner","lb-left-takedown","lb-front-blue-1","lb-front-blue-2","lb-front-red-2","lb-front-red-1","lb-right-takedown","lb-right-front-corner","lb-right-back-corner","lb-back-red-1","lb-back-red-2","lb-back-red-3","lb-back-blue-3","lb-back-blue-2","lb-back-blue-1","lb-left-back-corner","lb-back-yellow-1","lb-back-yellow-2","lb-back-yellow-3","lb-back-yellow-4","lb-back-yellow-5","lb-back-yellow-6","lb-left-alley","lb-right-alley","lb-ion-blue","lb-ion-red","radar_patrol_c","radar_patrol_d","radar_patrol_u","radar_fast_c","radar_fast_d","radar_fast_u","radar_target_c","radar_target_d","radar_target_u","Camo1"};
-	hiddenSelectionsTextures[] = {"","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","TEXTUREHERESAM",""};
+	hiddenSelectionsTextures[] = {"","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","\Lannister\Vehicles\BMW_M5\Red.paa",""};
+};
+
+class A3LRP_LanV_White_BMW_M5 : BMW_M5
+{
+	scope = 2;
+	displayName = "A3LRP BMW M5 (House Lannister Red)";
+	hiddenSelections[] = {"jump","lb-left-front-corner","lb-left-takedown","lb-front-blue-1","lb-front-blue-2","lb-front-red-2","lb-front-red-1","lb-right-takedown","lb-right-front-corner","lb-right-back-corner","lb-back-red-1","lb-back-red-2","lb-back-red-3","lb-back-blue-3","lb-back-blue-2","lb-back-blue-1","lb-left-back-corner","lb-back-yellow-1","lb-back-yellow-2","lb-back-yellow-3","lb-back-yellow-4","lb-back-yellow-5","lb-back-yellow-6","lb-left-alley","lb-right-alley","lb-ion-blue","lb-ion-red","radar_patrol_c","radar_patrol_d","radar_patrol_u","radar_fast_c","radar_fast_d","radar_fast_u","radar_target_c","radar_target_d","radar_target_u","Camo1"};
+	hiddenSelectionsTextures[] = {"","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","\Lannister\Vehicles\BMW_M5\White.paa",""};
 };
--- Lannister/Vehicles/MercG65/config.hpp
+++ Lannister/Vehicles/MercG65/config.hpp
@@ -5,3 +5,19 @@ class A3LRP_LanV_Red_MercG65 : MercG65
 	hiddenSelections[] = {"camo1","red","blue","lock1","lock2","lock3","target1","target2","target3","patrol1","patrol2","patrol3","police","stay","back","volumeblue","volumered"};
 	hiddenSelectionsTextures[] = {"\Lannister\Vehicles\MercG65\Red.paa"};
 };
+
+class A3LRP_LanV_Red_MercG65 : MercG65
+{
+	scope = 2;
+	displayName = "A3LRP Mercedes-Benz G65 AMG (House Lannister Kryptec)";
+	hiddenSelections[] = {"camo1","red","blue","lock1","lock2","lock3","target1","target2","target3","patrol1","patrol2","patrol3","police","stay","back","volumeblue","volumered"};
+	hiddenSelectionsTextures[] = {"\Lannister\Vehicles\MercG65\Kryptec.paa"};
+};
+
+class A3LRP_LanV_Red_MercG65 : MercG65
+{
+	scope = 2;
+	displayName = "A3LRP Mercedes-Benz G65 AMG (House Lannister White)";
+	hiddenSelections[] = {"camo1","red","blue","lock1","lock2","lock3","target1","target2","target3","patrol1","patrol2","patrol3","police","stay","back","volumeblue","volumered"};
+	hiddenSelectionsTextures[] = {"\Lannister\Vehicles\MercG65\White.paa"};
+};
